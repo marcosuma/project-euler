5c7fcd4118142f3e1643dd9c25d89d2fac471c26
Marco Suma <marcsuma@amazon.com>
2019-02-13 19:33:19 +0000

    Implement final solution for project-euler 243 https://projecteuler.net/problem\=243

--- Problem243/problem.rb
+++ Problem243/problem.rb
@@ -9,66 +9,62 @@
 # Find the smallest denominator d, having a resilience R(d) < 15499/94744 .
 
 
-def gcd(a, b)
-  if a == b
-    return [a]
-  end
-
-  list = {}
-  while a != b
-    if a > b
-      list[[a-b, b].max] = true
-      a = a - b
-    elsif a < b
-      list[[b-a, a].max] = true
-      b = b - a
+def find_next_prime(prev_p)
+  p = prev_p + 1
+  while true
+    i = 2
+    prime = true
+    while i < p
+      if p % i == 0
+        prime = false
+        break
+      end
+      i = i + 1
     end
+    if prime
+      break
+    end
+    p = p + 1
   end
-
-  if a == 1
-    return {}
-  end
-  return list
+  p
 end
 
-# puts "#{gcd(76406500, 625)}"
-
-target = 15499
-d = 94744
-count = d - 1
+# Credits to https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p243.py which helped me finding the solution to the problem.
+#
+# The algorithm is based on the concept of euler's totient function: https://en.wikipedia.org/wiki/Euler%27s_totient_function
+# It can be demonstrated that d is a potential solution iff it is composed by all prime numbers between 1 and a certain value n
+# If a value d is missing even a prime number between 1 and n, then for sure it won't be part of the solution.
+# We can demonstrate that by calculating the totient function of two numbers d and d' with these two different characteristics
+# and then we can demonstrate that R(d) is a non-decreasing function therefore what we are saying is true.
+# The obstacle is that we don't know with what frequency each prime number is occurring in the d value, which is the solution to the problem.
+# So we start defining an equivalent R'(d) function that helps defining us the lower boundary for the solution d.
+# From that value we keep trying multiplying all numbers by iterating from 2 to p, which is the biggest prime numbers found in the potential solution d.
+NUM = 15499.0
+DEN = 94744.0
+p = 1
+d = 1
+totient = 1
 while true
-  # puts "d: #{d}"
-  i = 2
-  excludedNumbers = {}
-  while i < d / 2 + 1
-    if excludedNumbers[i] != true
+  p = find_next_prime(p)
+  d = d * p
+  totient = totient * (p - 1)
+  if totient.to_f / d < (NUM * 1.0 / DEN)
+    found = false
+    i = 2
+    while i < p
+      num = totient * i
+      den = d * i
+      if num.to_f / (den - 1) < (NUM * 1.0 / DEN)
+        puts "#{den}"
+        found = true
+        break
+      end
 
-      list = gcd(d, i)
-      count = count - list.size
-      # puts "#{i}: #{list} --> count = #{count}"
-      excludedNumbers.merge! list
+      i = i + 1
     end
 
-
-    i = i + 1
+    if found
+      break
+    end
   end
-
-  puts "count is #{count}"
-  puts "d is #{d}"
-  if count <= target
-    break
-  end
-  # 0.163588195558558
-  # if (count * 1.0 / d) < 0.1635
-  #   puts "breaking.. #{count / d}"
-  #   break
-  # end
-
-  d = d + 1
-  count = d - 1
 end
-
-puts "#{count}"
-puts "#{d}"
-
-
